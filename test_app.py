bff825e03d4c9d18dfe109721eb6d558ce07e990
Kittisak Sajjapongse <kittisak.sajjapongse@gmail.com>
2024-05-06 10:40:23 -0400

    added subtraction

--- test_app.py
+++ test_app.py
@@ -1,7 +1,11 @@
 import pytest
 
-from app import addition
+from app import addition, subtraction
 
 def test_addition():
     assert 4 == addition(2, 2)
     assert 100 == addition(70, 30)
+
+def test_subtraction():
+    assert 50 == subtraction(500, 450)
+    assert 300 == subtraction(821, 521)
